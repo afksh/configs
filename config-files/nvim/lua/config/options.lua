882d93b5aa116505b380fde1e5143818685248e0
AFK <51038016+afksh@users.noreply.github.com>
2026-07-10 20:00:00 -0700

    auto-sync: 2026-07-10 20:00

---
+++ config-files/nvim/lua/config/options.lua
@@ -0,0 +1,20 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Indentation (override LazyVim's default of 2)
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+-- Visual
+vim.opt.encoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.cursorcolumn = true
+vim.opt.colorcolumn = "80"
+vim.opt.showmatch = true
+vim.opt.title = true
+vim.opt.scrolloff = 8
+
+-- Clipboard (sync with macOS clipboard)
+vim.opt.clipboard = "unnamedplus"
