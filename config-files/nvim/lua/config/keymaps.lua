ac228455bb5664f1edf0082cc87c5b7da139fd95
AFK <51038016+afksh@users.noreply.github.com>
2026-07-12 11:00:00 -0700

    auto-sync: 2026-07-12 11:00

--- config-files/nvim/lua/config/keymaps.lua
+++ config-files/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Half-page scrolling with Shift+Up/Down
+vim.keymap.set('n', '<S-Up>', '<C-u>', { noremap = true, desc = 'Half-page up' })
+vim.keymap.set('n', '<S-Down>', '<C-d>', { noremap = true, desc = 'Half-page down' })
