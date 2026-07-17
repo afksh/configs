4e36c3356f49fe16d3871d7455ca025d5601b34c
AFK <51038016+afksh@users.noreply.github.com>
2026-07-16 21:00:01 -0700

    auto-sync: 2026-07-16 21:00

--- config-files/nvim/lua/config/keymaps.lua
+++ config-files/nvim/lua/config/keymaps.lua
@@ -5,3 +5,13 @@
 -- Half-page scrolling with Shift+Up/Down
 vim.keymap.set('n', '<S-Up>', '<C-u>', { noremap = true, desc = 'Half-page up' })
 vim.keymap.set('n', '<S-Down>', '<C-d>', { noremap = true, desc = 'Half-page down' })
+
+-- Save on escape, on top of LazyVim's default clear-hlsearch/stop-snippet behavior
+vim.keymap.set({ 'i', 'n', 's' }, '<esc>', function()
+  vim.cmd('noh')
+  LazyVim.cmp.actions.snippet_stop()
+  if vim.bo.modifiable and vim.bo.buftype == '' and vim.fn.expand('%') ~= '' then
+    vim.cmd('silent! update')
+  end
+  return '<esc>'
+end, { expr = true, desc = 'Escape, Clear hlsearch, and Save' })
---
+++ config-files/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+    opts = {},
+  },
+}
